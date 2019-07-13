52d6fbce5bfe19e437ddec0654ff1a5e393a2677
ying li <yingli@yingdeMacBook-Air.local>
2019-07-13 17:56:26 +0800

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Nancy's first website</h1>
+    <h2>Nancy is smart</h2>
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  },
+  methods:{
+
+  },
+  onload(){
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
